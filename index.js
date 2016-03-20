8fcfc4be95ea545ca91d4dca45d8eb3fa12f2aac
prince-0203 <mc.prince.0203@gmail.com>
2016-03-20 16:12:58 +0900

    Revert "OpenShift Test 3"
    This reverts commit ec6edb39a80b4727bf4fe4c1da327c0bf35b12bd.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 const ipAddress = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP,
-port = 8080;
+port = process.env.OPENSHIFT_NODEJS_PORT || process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
 if (!ipAddress) {
   console.warn('No OPENSHIFT_*_IP var, using 127.0.0.1');
