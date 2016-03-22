e06f4f905c1cf140f7cfea95dbb209c7ec22a78b
prince-0203 <mc.prince.0203@gmail.com>
2016-03-22 18:16:29 +0900

    Fix typo

--- command/feedback.js
+++ command/feedback.js
@@ -3,6 +3,6 @@
  */
 
 module.exports = (args, client, originalTweet, callback) => {
-  client.post('statuses/update', { status: `@prince__0203 Feedback: https://twitter.com/${oroginalTweet.user.screen_name}/${originalTweet.id_str}` });
+  client.post('statuses/update', { status: `@prince__0203 Feedback: https://twitter.com/${originalTweet.user.screen_name}/status/${originalTweet.id_str}` });
   return callback('管理者にツイートが送信されました。');
 };
