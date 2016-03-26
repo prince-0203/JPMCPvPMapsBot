21f6d34a5674757a5cfa52baab0b7573c8a337f0
prince-0203 <mc.prince.0203@gmail.com>
2016-03-26 10:12:31 +0900

    会話機能を実装完了

--- index.js
+++ index.js
@@ -23,14 +23,7 @@ if(process.env.OPENSHIFT_APP_NAME) {
   OpenShiftServer();
 }
 
-const mysqlConnection = mysql.createConnection(process.env.OPENSHIFT_MYSQL_DB_URL);
-
-mysqlConnection.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log('Connected to MySQL: ' + mysqlConnection.threadId);
-});
+const mysqlPool = mysql.createPool(process.env.OPENSHIFT_MYSQL_DB_URL);
 
 if(process.env.LOCAL_DEBUG === '1') {
   console.warn('Local debug mode!');
@@ -120,45 +113,89 @@ if(process.env.LOCAL_DEBUG === '1') {
   stream.on('direct_message', function(data) {
     const message = data.direct_message;
 
-    console.log(`Received direct message from @${message.sender.screen_name}: ${message.text}`);
-
     if(message.sender.id_str === botInfo.id) {
       // 無限ループこわい
       return;
     }
 
-    request.post({
-      uri: 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue',
-      timeout: 5000,
-      json: true,
-      qs: {
-        APIKEY: process.env.DOCOMO_API_KEY
-      },
-      body: {
-        utt: message.text,
-        nickname: message.sender.name
-      }
-    }, (err, res, dialogue) => {
-      if(err || res.statusCode !== 200) {
+    console.log(`Received direct message from @${message.sender.screen_name}: ${message.text}`);
+
+    // DBからユーザーデータを取得
+    const getUserData = (callback) => {
+      mysqlPool.query({
+        sql: 'SELECT * FROM `userdata` WHERE `userid` = ?',
+        timeout: 1000 * 5,
+        values: [message.sender.id_str]
+      },  (err, rows) => {
         if(err) {
-          console.error(err);
+          throw err;
         }
-        twit.post('direct_messages/new', {
-          user_id: message.sender.id_str,
-          text: '内部エラー: 雑談対話APIからデータを取得できませんでした。'
-        });
-      } else {
-        twit.post('direct_messages/new', {
-          user_id: message.sender.id_str,
-          text: dialogue.utt
-        }, (err) => {
-          if (err) {
+        if(rows.length !== 0) {
+          return callback(rows[0]['dialog-context'] ? rows[0]['dialog-context'] : undefined, rows[0]['dialog-mode'] ? rows[0]['dialog-mode'] : undefined);
+        } else {
+          mysqlPool.query({
+            sql: 'INSERT INTO `userdata` set ?',
+            values: { userid: message.sender.id_str }
+          }, (err) => {
+            if(err) {
+              throw err;
+            }
+            return callback(undefined, undefined);
+          });
+        }
+      });
+    };
+
+    getUserData((context, mode) => {
+      console.log(context, mode);
+      request.post({
+        uri: 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue',
+        timeout: 5000,
+        json: true,
+        qs: {
+          APIKEY: process.env.DOCOMO_API_KEY
+        },
+        body: {
+          utt: message.text,
+          nickname: message.sender.name,
+          context: context,
+          mode: mode
+        }
+      }, (err, res, dialog) => {
+        if(err || res.statusCode !== 200) {
+          if(err) {
             console.error(err);
-          } else {
-            console.log(`Replied direct message: ${dialogue.utt}`);
           }
-        });
-      }
+          twit.post('direct_messages/new', {
+            user_id: message.sender.id_str,
+            text: '内部エラー: 雑談対話APIからデータを取得できませんでした。'
+          });
+        } else {
+          // データをDBに保存(非同期)
+          mysqlPool.query({
+            sql: 'UPDATE `userdata` SET `dialog-context` = ?, `dialog-mode` = ? WHERE `userid` = ?',
+            values: [
+              dialog.context, dialog.mode, message.sender.id_str
+            ]
+          }, (err) => {
+            if(err) {
+              throw err;
+            }
+          });
+
+          // 返信
+          twit.post('direct_messages/new', {
+            user_id: message.sender.id_str,
+            text: dialog.utt
+          }, (err) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log(`Replied direct message: ${dialog.utt}(Context: ${dialog.context}, Mode: ${dialog.mode})`);
+            }
+          });
+        }
+      });
     });
   });
 
