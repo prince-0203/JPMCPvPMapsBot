2b2694a24c2c404f77d139420d43ffaa346a2f31
prince-0203 <mc.prince.0203@gmail.com>
2017-03-07 19:31:03 +0900

    R.I.P.

--- command/about.js
+++
@@ -1,9 +0,0 @@
-/**
- * about command
- */
-
-const config = require('../package.json');
-
-module.exports = (callback) => {
-  return callback(`JPMCPvP Maps Bot v${config.version}\nAuthor: prince(https://github.com/prince-0203) TwitterID: prince__0203\n(c) prince 2016`);
-};
